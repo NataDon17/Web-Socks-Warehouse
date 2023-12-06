20c91dc1bdc57a55a612b5a65053bffccce0440d
https://github.com/NataDon17/Course2-Homework-11.git <don-nata@mail.ru>
2023-12-06 11:50:33 +0300

    initial commit

---
+++ src/main/java/org/example/websockswarehouse/WebSocksWarehouseApplication.java
@@ -0,0 +1,13 @@
+package org.example.websockswarehouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebSocksWarehouseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebSocksWarehouseApplication.class, args);
+	}
+
+}
